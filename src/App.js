86aa2f16dd4959fea96fd5c78dc4f002d7287128
HuzefaUsama25 <huzefausama25@gmail.com>
2021-11-27 23:22:15 +0500

    Removed continuos operator display bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Calculator from "./components/Calculator";
 function App() {
   return (
     <div className="App">
-      <h1>World's 1 Calculator</h1>
+      <h1>World's #1 Calculator</h1>
       <br />
       <Calculator />
     </div>
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,14 +3,23 @@ import {useState} from 'react'
 
 export default function Calculator() {
     const [display, setdisplay] = useState("");
+    const operators = ["+","-","*","/","."]
+    const digits = ["1","2","3","4","5","6","7","8","9","0"]
 
 
     function parse(str) {
-        return Function(`'use strict'; return (${str})`)()
+        return eval(str)
     }
 
-    function btnTapped(value) {
-        setdisplay(display + value)
+    function btnTapped(value) { 
+        if (operators.includes(display.slice(-1)) && operators.includes(value)){
+            console.log("Error!")
+        }
+
+        else {
+            setdisplay(display + value)
+        }
+        
     }
 
     function setEqual() {
@@ -31,24 +40,24 @@ export default function Calculator() {
                 {display ? display : 0}
             </div>
             <div className="keypad">
-                <button onClick={() => btnTapped(1)}>1</button>
-                <button onClick={() => btnTapped(2)}>2</button>
-                <button onClick={() => btnTapped(3)}>3</button>
-                <button className="addition operator" onClick={() => btnTapped("+")}>+</button>
-                <button onClick={() => btnTapped(4)}>4</button>
-                <button onClick={() => btnTapped(5)}>5</button>
-                <button onClick={() => btnTapped(6)}>6</button>
-                <button className="subtraction operator" onClick={() => btnTapped("-")}>-</button>
-                <button onClick={() => btnTapped(7)}>7</button>
-                <button onClick={() => btnTapped(8)}>8</button>
-                <button onClick={() => btnTapped(9)}>9</button>
-                <button className="multiplication operator" onClick={() => btnTapped("*")}>x</button>
-                <button onClick={() => btnTapped(0)}>0</button>
+                <button onClick={() => btnTapped('1')}>1</button>
+                <button onClick={() => btnTapped('2')}>2</button>
+                <button onClick={() => btnTapped('3')}>3</button>
+                <button className="addition operator" onClick={() => btnTapped("+")}>&#x2b;</button>
+                <button onClick={() => btnTapped('4')}>4</button>
+                <button onClick={() => btnTapped('5')}>5</button>
+                <button onClick={() => btnTapped('6')}>6</button>
+                <button className="subtraction operator" onClick={() => btnTapped("-")}>&minus;</button>
+                <button onClick={() => btnTapped('7')}>7</button>
+                <button onClick={() => btnTapped('8')}>8</button>
+                <button onClick={() => btnTapped('9')}>9</button>
+                <button className="multiplication operator" onClick={() => btnTapped("*")}>&times;</button>
+                <button onClick={() => btnTapped('0')}>0</button>
                 <button className="clearC" onClick={() => clearDisplay()}>C</button>
                 <button className="clearCE" onClick={() => backspace()}>CE</button>
 
 
-                <button className="division operator" onClick={() => btnTapped("/")}>/</button>
+                <button className="division operator" onClick={() => btnTapped("/")}>&divide;</button>
                 <button onClick={() => btnTapped(".")}>.</button>
 
                 <button className="equal operator" onClick={() => setEqual()}>=</button>
