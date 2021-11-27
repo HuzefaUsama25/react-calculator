685de675fdaf8e4c7d2baf7f96a1f3e1d14ecbf3
HuzefaUsama25 <huzefausama25@gmail.com>
2021-11-27 14:07:12 +0500

    improved css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import { useState } from "react"
 
 
 function App() {
@@ -10,49 +10,55 @@ function App() {
     return Function(`'use strict'; return (${str})`)()
   }
 
-  function btnTapped (value) {
+  function btnTapped(value) {
     setdisplay(display + value)
   }
 
-  function setEqual () {
+  function setEqual() {
     setdisplay(Math.round(parse(display)));
   }
-  
+
   function clearDisplay() {
     setdisplay("0")
   }
 
   function backspace() {
     let text = display.toString()
-    setdisplay(text.slice(0, text.length-1))
+    setdisplay(text.slice(0, text.length - 1))
   }
 
   return (
-    <div className= "calculator">
-      <div className="display">
-        {display ? display : 0}
-      </div>
-      <div className="keypad">
-      <button onClick={()=>btnTapped(1)}>1</button>
-      <button onClick={()=>btnTapped(2)}>2</button>
-      <button onClick={()=>btnTapped(3)}>3</button> 
-      <button onClick={()=>btnTapped(4)}>4</button> 
-      <button onClick={()=>btnTapped(5)}>5</button> 
-      <button onClick={()=>btnTapped(6)}>6</button> 
-      <button onClick={()=>btnTapped(7)}>7</button> 
-      <button onClick={()=>btnTapped(8)}>8</button> 
-      <button onClick={()=>btnTapped(9)}>9</button> 
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          {display ? display : 0}
+        </div>
+        <div className="keypad">
+          <button onClick={() => btnTapped(1)}>1</button>
+          <button onClick={() => btnTapped(2)}>2</button>
+          <button onClick={() => btnTapped(3)}>3</button>
+          <button onClick={() => btnTapped(4)}>4</button>
+          <button onClick={() => btnTapped(5)}>5</button>
+          <button onClick={() => btnTapped(6)}>6</button>
+          <button onClick={() => btnTapped(7)}>7</button>
+          <button onClick={() => btnTapped(8)}>8</button>
+          <button onClick={() => btnTapped(9)}>9</button>
+
+          <button onClick={() => btnTapped(0)}>0</button>
+
+          <button onClick={() => clearDisplay()}>C</button>
+          <button onClick={() => backspace()}>CE</button>
+          <button onClick={() => btnTapped(".")}>.</button>
 
 
-      <button onClick={()=>btnTapped("+")}>+</button> 
-      <button onClick={()=>btnTapped("-")}>-</button> 
-      <button onClick={()=>btnTapped("*")}>*</button> 
-      <button onClick={()=>btnTapped("/")}>/</button> 
+          <button onClick={() => btnTapped("+")}>+</button>
+          <button onClick={() => btnTapped("-")}>-</button>
+          <button onClick={() => btnTapped("*")}>*</button>
+          <button onClick={() => btnTapped("/")}>/</button>
 
-      <button onClick={()=>setEqual()}>=</button> 
-      <button onClick={()=>clearDisplay()}>C</button>
-      <button onClick={()=>backspace()}>CE</button>
+          <button onClick={() => setEqual()}>=</button>
 
+        </div>
       </div>
     </div>
   );
